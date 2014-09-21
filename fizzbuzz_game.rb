f736e7c1f543f939e87a27c5b7a618fc87a50810
katherineabuhadal <katherine8@verizon.net>
2014-09-21 13:36:52 -0400

    Add constraints to fizzbuzz game

--- fizzbuzz_game.rb
+++ fizzbuzz_game.rb
@@ -1,32 +1,48 @@
 class FizzbuzzGame
+  def initialize
+    @test_case_variables = []
+  end
+
   def play
     prompt_user
-    assign_variables
-    print_numbers
+    parse_user_input
   end
 
   def prompt_user
-    print "Please enter three different numbers, separated by a space (e.g. '3 5 100') "
-    @user_input = gets.chomp
+    print "Please enter three different numbers, separated by a space (e.g. '3 5 100'). Type 'END' when you finish. "
+    $/ = "END"
+    @user_input = STDIN.gets.chomp
   end
 
-  def assign_variables
-    variables = @user_input.split(" ").map { |s| s.to_i }
-    @first_divisor = variables[0]
-    @second_divisor = variables[1]
-    @last_value_in_range = variables[2]
+  def parse_user_input
+    test_cases = @user_input.split("\n")
+    test_cases.each do |test_case|
+      @test_case_variables << test_case.split(" ").map { |s| s.to_i }
+    end
+    if test_cases.count <= 20
+      check_constraints
+    else
+      puts "Sorry you may only do 20 cases at a time"
+    end
   end
 
-  def print_numbers
-    (1..@last_value_in_range).each do |n|
-      if n % @first_divisor == 0 && n % @second_divisor == 0
-        puts "FB"
-      elsif n % @first_divisor == 0
-        puts "F"
-      elsif n % @second_divisor == 0
-        puts "B"
+  def check_constraints
+    @test_case_variables.each do |variable|
+      if (1..20).include?(variable[0] && variable[1]) && (21..100).include?(variable[2])
+        (1..variable[2]).each do |n|
+          if n % variable[1] == 0 && n % variable[0] == 0
+            print "FB "
+          elsif n % variable[0] == 0
+            print "F "
+          elsif n % variable[1] == 0
+            print "B "
+          else
+            print n.to_s + " "
+          end
+        end
+        print "\n"
       else
-        puts n
+        puts "Sorry, the first two numbers must be between 1 and 20 and the last number should be between 21 and 100"
       end
     end
   end
